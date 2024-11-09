944a885fe1048c52ed602bb989cdd3f0a45c8dea
Jeremy <look@jeremyfletcher.com>
2024-11-09 12:33:09 -0800

    fix: use React.forwardRef for EmojiCard component

--- src/components/EmojiCard.js
+++ src/components/EmojiCard.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 import Link from 'next/link';
 import { generateSlug } from '@/lib/emojiUtils';
 
-const EmojiCard = forwardRef(({ emoji }, ref) => {
+const EmojiCard = React.forwardRef(({ emoji }, ref) => {
   return (
     <div ref={ref} className="emoji-item bg-gray-100 rounded-lg p-2 m-2 w-52" title={emoji.name}>
       <Link href={`/emoji/${generateSlug(emoji.name)}`}>
